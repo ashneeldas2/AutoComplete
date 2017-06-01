4c5fc7b3a81326baaef9727cecbce9e3d77b0bec
ashneeldas2 <adas2@stuy.edu>
2017-06-01 16:26:31 -0400

    added methods

--- Term.java
+++ Term.java
@@ -1,4 +1,15 @@
-public class Term {
 
+public class Term implements Comparable<Term> {
 
+    public Term(String query, long weight)
+
+    public static Comparator<Term> byReverseWeightOrder()
+
+    public static Comparator<Term> byPrefixOrder(int r)
+
+    public int compareTo(Term that)
+
+    public String toString()
+
+    public static void main(String[] args)   
 }
