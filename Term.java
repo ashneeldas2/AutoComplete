3bfa60094ec712fd064a03e475fac09adc9c16da
ashneeldas2 <adas2@stuy.edu>
2017-06-01 22:05:23 -0400

    methods stubs

--- Term.java
+++ Term.java
@@ -1,15 +1,21 @@
-
 public class Term implements Comparable<Term> {
 
+    // Initializes a term with the given query string and weight.
     public Term(String query, long weight)
 
+    // Compares the two terms in descending order by weight.
     public static Comparator<Term> byReverseWeightOrder()
 
+    // Compares the two terms in lexicographic order but using only the first r characters of each query.
     public static Comparator<Term> byPrefixOrder(int r)
 
+    // Compares the two terms in lexicographic order by query.
     public int compareTo(Term that)
 
+    // Returns a string representation of this term in the following format:
+    // the weight, followed by a tab, followed by the query.
     public String toString()
 
+    // unit testing (required)
     public static void main(String[] args)   
 }
