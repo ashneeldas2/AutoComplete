8cfc9ebfe827656c735fd77ca69112525d04e08b
Anish Shenoy <shenoy.anish.227@gmail.com>
2017-06-03 19:07:20 -0400

    Update AutoComplete.java

--- AutoComplete.java
+++ AutoComplete.java
@@ -21,7 +21,7 @@ public class Autocomplete {
     }
 
     // Returns all terms that start with the given prefix, in descending order of weight.
-    public Term[] allMatches(String prefix)
+    public ArrayList<Term> allMatches(String prefix)
 
     // Returns the number of terms that start with the given prefix.
     public int numberOfMatches(String prefix)
