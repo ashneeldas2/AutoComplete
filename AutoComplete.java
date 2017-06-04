97c1499eb30c3003daec53f1b258ed8811b599e7
Anish Shenoy <shenoy.anish.227@gmail.com>
2017-06-03 23:44:56 -0400

    Update AutoComplete.java

--- AutoComplete.java
+++ AutoComplete.java
@@ -6,22 +6,35 @@ public class Autocomplete {
 	//2) Sort _words
 	//3) Search _words using binary search for matching results. 
 	//4) Put the found words into the priority queue. 
-	public PriorityQueue<Term> _results;
-	public ArrayList<Term> _words;
+	private PriorityQueue<Term> _results;
+	private ArrayList<Term> _words;
+	private String _phrase; 
 
-    // Initializes the data structure from the given array of terms.
-    public Autocomplete(ArrayList<Term> words){
+	// Initializes the data structure from the given array of terms.
+    public Autocomplete(ArrayList<Term> words, String phrase){
     	_results = new PriorityQueue<Term>();
     	_words = words;
+		_phrase = phrase;
     }
     
     // Sort all words in file (_words)
     public void sortFile(){
-    
+		
     }
 
     // Returns all terms that start with the given prefix, in descending order of weight.
-    public ArrayList<Term> allMatches(String prefix)
+    public PriorityQueue<Term> allMatches(String prefix){
+	    
+    }
+
+	
+    public int getFirstOccur(String prefix){
+	    
+    }
+ 
+    public int getLastOccur(String prefix){
+	    
+    }
 
     // Returns the number of terms that start with the given prefix.
     public int numberOfMatches(String prefix)
