d49e6971a95626a7badb9f305569702e49a56638
Anish Shenoy <shenoy.anish.227@gmail.com>
2017-06-11 22:13:10 -0400

    Create ReverseWeightComparator.java

--- ReverseWeightComparator.java
+++ ReverseWeightComparator.java
@@ -5,6 +5,7 @@ public class ReverseWeightComparator<T> implements Comparator<T>{
 
    public int compare(T a, T b) throws ClassCastException{
 	   long weightDiff = ((Term) b).getWeight() - ((Term) a).getWeight();
+       // Accounts for Integer Overlflow
        if (weightDiff > 0) return 1;
        else if (weightDiff < 0) return -1;
        else return 0;
